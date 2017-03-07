2429149878e8748e35b2ba8934e79dcdb9222f94
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2017-03-07 13:13:00 +0100

    Correct stack-work path

--- ci/tasks/compile.sh
+++ ci/tasks/compile.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-STACK_WORK=$pwd/stack-work
+STACK_WORK=$PWD/stack-work
 
 pushd cpi-src/src/bosh-kubernetes-cpi
   stack build --work-dir $STACK_WORK
--- ci/tasks/test-unit.sh
+++ ci/tasks/test-unit.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-STACK_WORK=$pwd/stack-work
+STACK_WORK=$PWD/stack-work
 
 pushd cpi-src/src/bosh-kubernetes-cpi
   stack test --work-dir $STACK_WORK
