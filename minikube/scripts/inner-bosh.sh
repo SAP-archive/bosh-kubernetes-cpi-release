9456b25da5a6063dc28a4a37afe33ad91302c810
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2017-03-12 01:22:32 +0100

    Improve scripts

--- minikube/scripts/inner-bosh.sh
+++ minikube/scripts/inner-bosh.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+set -e
+
+
+BASE_DIR=${BASE_DIR:-./bosh-on-k8s}
+BOSH_DEPLOYMENT=${BOSH_DEPLOYMENT:-$BASE_DIR/bosh-deployment}
+echo "Working directory: $BASE_DIR"
 
 mkdir -p bosh-on-k8s
 pushd bosh-on-k8s
+  if [ ! -d "$BOSH_DEPLOYMENT" ]; then
+    echo "Cloning bosh-deployment into: $BOSH_DEPLOYMENT"
+    git clone git@github.com:loewenstein/bosh-deployment.git
+    pushd bosh-deployment
+      git checkout origin/latest-bosh
+    popd
+  else
+    echo "Use existing bosh-deployment directory: $BOSH_DEPLOYMENT"
+  fi
+  echo "Uploading releases..."
   bosh -e outer-bosh \
       upload-release \
       https://bosh.io/d/github.com/cloudfoundry/bosh?v=261.3 \
@@ -12,38 +28,49 @@ pushd bosh-on-k8s
       https://github.com/SAP/bosh-kubernetes-cpi-release/releases/download/v3-alpha/bosh-kubernetes-cpi-3-alpha.tgz \
       --client admin \
       --client-secret $(bosh int ./outer-bosh/creds.yml --path /admin_password)
+
+  echo "Uploading stemcell..."
   bosh -e outer-bosh \
       upload-stemcell \
       https://github.com/SAP/bosh-kubernetes-cpi-release/releases/download/v1-alpha/bosh-stemcell-3363.9-kubernetes-ubuntu-trusty-go_agent.tgz \
       --client admin \
       --client-secret $(bosh int ./outer-bosh/creds.yml --path /admin_password)
 
+  echo "Updating cloud-config..."
   bosh -n -e outer-bosh \
       update-cloud-config ~/projects/bosh-deployment/kubernetes/cloud-config.yml \
       --client admin \
       --client-secret $(bosh int ./outer-bosh/creds.yml --path /admin_password) \
-      -o ~/projects/bosh-deployment/kubernetes/bosh-cloud-config.yml \
-      -v port-blobstore=31250 \
-      -v port-director=31555 \
-      -v port-nats=31422
+      -o $BOSH_DEPLOYMENT/kubernetes/bosh-cloud-config.yml \
+      -o $BOSH_DEPLOYMENT/kubernetes/bosh-cloud-config-ports.yml \
+      -v port_blobstore=31250 \
+      -v port_director=31555 \
+      -v port_nats=31422 \
+      -v port_ssh_gateway=31022
 
+  mkdir -p inner-bosh
+  if [ ! -f inner-bosh/bosh.key.pub ]; then
+    ssh-keygen -t rsa -b 4096 -C "vcap" -N "" -f inner-bosh/bosh.key
+  fi
+  echo "Deploying inner bosh"
   bosh -n -e outer-bosh \
-      deploy ~/projects/bosh-deployment/bosh.yml \
+      deploy $BOSH_DEPLOYMENT/bosh.yml \
       -d bosh \
-      -o ~/projects/bosh-deployment/kubernetes/cpi.yml \
-      -o ~/projects/bosh-deployment/bosh-dev.yml \
-      -o ~/projects/bosh-deployment/kubernetes/cpi-inner.yml \
-      -o ~/projects/bosh-deployment/local-dns.yml \
+      -o $BOSH_DEPLOYMENT/kubernetes/cpi.yml \
+      -o $BOSH_DEPLOYMENT/bosh-dev.yml \
+      -o $BOSH_DEPLOYMENT/kubernetes/cpi-inner.yml \
+      -o $BOSH_DEPLOYMENT/kubernetes/cpi-inner-ports.yml \
+      -o $BOSH_DEPLOYMENT/local-dns.yml \
       --client admin \
       --client-secret $(bosh int ./outer-bosh/creds.yml --path /admin_password) \
       --vars-store ./inner-bosh/creds.yml \
+      --var-file=vcap_public_key=inner-bosh/bosh.key.pub \
       -v internal_ip=$(minikube ip) \
       -v director_name=inner-bosh \
-      -v host_ip=$(minikube ip) \
-      -v namespace=default \
-      -v port-blobstore=31250 \
-      -v port-nats=31422
+      -v port_blobstore=31250 \
+      -v port_nats=31422
 
+  echo "Creating env alias: inner-bosh"
   bosh -e $(minikube ip):31555 \
       --ca-cert <(bosh int ./inner-bosh/creds.yml --path /director_ssl/ca) \
       alias-env inner-bosh
--- minikube/scripts/outer-bosh.sh
+++ minikube/scripts/outer-bosh.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
+set -e
+
+
+BASE_DIR=${BASE_DIR:-./bosh-on-k8s}
+BOSH_DEPLOYMENT=${BOSH_DEPLOYMENT:-$BASE_DIR/bosh-deployment}
+echo "Working directory: $BASE_DIR"
 
 mkdir -p bosh-on-k8s
 pushd bosh-on-k8s
-  git clone git@github.com:loewenstein/bosh-deployment.git
-  pushd bosh-deployment
-    git checkout origin/latest-bosh
-  popd
-  mkdir outer-bosh
-  bosh create-env ~/projects/bosh-deployment/bosh.yml \
+  if [ ! -d "$BOSH_DEPLOYMENT" ]; then
+    echo "Cloning bosh-deployment into: $BOSH_DEPLOYMENT"
+    git clone git@github.com:loewenstein/bosh-deployment.git
+    pushd bosh-deployment
+      git checkout origin/kubernetes
+    popd
+  else
+    echo "Use existing bosh-deployment directory: $BOSH_DEPLOYMENT"
+  fi
+  mkdir -p outer-bosh
+  if [ ! -f outer-bosh/bosh.key.pub ]; then
+    ssh-keygen -t rsa -b 4096 -C "vcap" -N "" -f outer-bosh/bosh.key
+  fi
+  echo "Deploying outer bosh"
+  bosh create-env $BOSH_DEPLOYMENT/bosh.yml \
     --state ./outer-bosh/state.json \
-    -o ~/projects/bosh-deployment/kubernetes/cpi.yml \
-    -o ~/projects/bosh-deployment/local-dns.yml \
+    -o $BOSH_DEPLOYMENT/kubernetes/cpi.yml \
+    -o $BOSH_DEPLOYMENT/kubernetes/cpi-ports.yml \
+    -o $BOSH_DEPLOYMENT/local-dns.yml \
     --vars-store ./outer-bosh/creds.yml \
     -v director_name=outer-bosh \
     -v internal_ip=$(minikube ip) \
-    --var-file=client-cert=$(kubectl config view -o json | jq -r '.users[] | select(.name=="minikube") | .user."client-certificate"') \
-    --var-file=client-key=$(kubectl config view -o json | jq -r '.users[] | select(.name=="minikube") | .user."client-key"') \
-    -v host_ip=$(minikube ip) \
-    -v namespace=default \
-    -v port-blobstore=30250 \
-    -v port-bosh-init=30068 \
-    -v port-director=30555 \
-    -v port-nats=30422
+    --var-file=client_cert=$(kubectl config view -o json | jq -r '.users[] | select(.name=="minikube") | .user."client-certificate"') \
+    --var-file=client_key=$(kubectl config view -o json | jq -r '.users[] | select(.name=="minikube") | .user."client-key"') \
+    --var-file=vcap_public_key=outer-bosh/bosh.key.pub \
+    -v kube_api_server="https://$(minikube ip):8443" \
+    -v kube_namespace=default \
+    -v port_blobstore=30250 \
+    -v port_bosh_init=30068 \
+    -v port_director=30555 \
+    -v port_nats=30422 \
+    -v port_ssh_gateway=30022
 
+  echo "Creating env alias: outer-bosh"
   bosh -e $(minikube ip):30555 \
       --ca-cert <(bosh int ./outer-bosh/creds.yml --path /director_ssl/ca) \
       alias-env outer-bosh
