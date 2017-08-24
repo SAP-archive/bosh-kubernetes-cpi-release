89a11fb5fcd94f4f1aa9f83ece331fc9e6fde6d4
Jan von Loewenstein <jan.von.loewenstein@sap.com>
2017-08-24 10:18:08 +0200

    Speed up `build` job by caching haskell dependencies

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-export STACK_ROOT=/root/.stack
+# export STACK_ROOT=/root/.stack
+export STACK_ROOT=$PWD/stack-root
 
 pushd cpi-src/src/bosh-kubernetes-cpi
+  stack setup
+  stack build --work-dir=.stack-work-deps --only-dependencies
+  cp -R .stack-work-deps .stack-work
   stack build --test
 popd
